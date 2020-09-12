fa2dc9741e36e921bb5526f8a40ed7b90556f5b8
Nia Smith <niasmith94@outlook.com>
2020-09-12 13:52:38 -0400

    finished variable activites

--- 2-JS-basics/starter/script.js
+++ 2-JS-basics/starter/script.js
@@ -1,6 +1,29 @@
+// String
 var firstName = 'Nia';
 console.log(firstName);
 var lastName = 'Smith';
 var age = 57;
 console.log(lastName);
-console.log(age);
+// console.log(age);
+
+// Boolean
+var fullAge = true;
+console.log(fullAge);
+// undefined variable
+var job;
+console.log(job);
+
+job = 'Teacher';
+console.log(job);
+
+// variable names can't begin with a number
+// var 3years = 3;
+// correct versions
+var  _3years = 3;
+var  $3years = 3;
+var  e3years = 3;
+
+// Reserved keywords, seen below, can't be used a variable names
+// var function = 23;
+// var delete = 23;
+// var if = 23;
