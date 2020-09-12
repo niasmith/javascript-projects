9b2663fc48c45ca012a115d073df8e096aede827
Nia Smith <niasmith94@outlook.com>
2020-09-12 14:13:52 -0400

    basic activites

--- 2-JS-basics/starter/script.js
+++ 2-JS-basics/starter/script.js
@@ -1,4 +1,8 @@
-// String
+/*
+Variables and data types lecture
+*/
+/*
+// Strings
 var firstName = 'Nia';
 console.log(firstName);
 var lastName = 'Smith';
@@ -27,3 +31,39 @@ var  e3years = 3;
 // var function = 23;
 // var delete = 23;
 // var if = 23;
+*/
+
+// Type coercion
+// var firstName = 'John';
+// var age = 28;
+// console.log (firstName +' '+ age);
+
+// var job, isMarried;
+// job = 'Teacher';
+// isMarried = 'false';
+// console.log(firstName +' is a ' + age  + ' years old ' + job + '. Is he married? '+ isMarried);
+
+
+// Variable mutation
+// age = 'twenty eight';
+// job = 'Driver';
+
+// alert(firstName +' is a ' + age  + ' years old ' + job + '. Is he married? '+ isMarried);
+
+// var lastName = prompt('What is his last name?');
+// console.log(firstName + '  ' + lastName);
+
+
+// Basic operators
+
+
+var yearJohn = 2020 -28;
+var yearMark = 2020- 33;
+console.log(yearJohn);
+console.log(yearMark);
+
+var year = 2020
+var yearNia = year - 26;
+var yearMark = year - 33;
+console.log(yearNia);
+console.log(yearMark);
