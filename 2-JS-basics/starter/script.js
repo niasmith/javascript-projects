d76c17ee114ffe9b322ea293579adb8a23314feb
Nia Smith <niasmith94@outlook.com>
2020-12-02 14:06:52 -0500

    script js 2

--- 2-JS-basics/starter/script.js
+++ 2-JS-basics/starter/script.js
@@ -97,4 +97,16 @@ var  e3years = 3;
 // console.log(typeof 'Mark is older than John');
 
 
-// Operator predence
+// Operator precedence
+
+var now = 2018;
+var yearJohn = 1989;
+var fullAge = 18;
+// Multiple operators
+var isFullAge = now - yearJohn >= fullAge;  //true
+console.log(isFullAge);
+// Grouping
+var ageJohn = now - yearJohn;
+var ageMark = 35;
+var average = (ageJohn + ageMark) /2;
+console.log(average);
